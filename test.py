4e29d6e5c1f8bc56aa3f9270e75ea6e3f39a34fe
Jeong-Hyun-Su <42924998+Jeong-Hyun-Su@users.noreply.github.com>
2018-11-08 02:31:46 +0900

    데이터셋 안의 인덱스 값 이미지를 출력해주는 프로그램 이미지가 정상적으로 저장이 되있나 확인하거나 해당 인덱스가 우리가 원하는 이미지인지 확인하는 용도로 사용하면 됌

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 import tensorflow as tf
 
 
+
 x = tf.placeholder(tf.float32, [None, 784])
 W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
